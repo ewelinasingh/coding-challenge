a66ff839c3544915e6c0694c94fd6b786dcc25f2
ewelinasingh <ewelinasingh96@gmail.com>
2019-11-09 21:14:30 +0000

    Create function to allow user to choose new coordinates for the ship.

--- script.js
+++ script.js
@@ -7,22 +7,32 @@ let mapheight = 5;
 
 window.onload = function(){
     document.getElementById('mapbutton').addEventListener('click', e =>{
-        document.getElementById('map').innerHTML = displayMap();
-        // Run displayMap function when button is pressed
+        document.getElementById('map').innerHTML = displayMap(); // Run displayMap function when button is pressed
     });
     document.getElementById('sizebutton').addEventListener('click', e =>{
-        let str = document.getElementById('mapsize').value;
-        // Get value from the text box
+        let str = document.getElementById('mapsize').value; // Get value from the text box
         setGrid(str); //Run setGrid function and pass str 
         document.getElementById('map').innerHTML = displayMap();
         document.getElementById('sizediv').style.display="none"; //Hide text box
     });
+    document.getElementById('coordsbutton').addEventListener('click', e =>{
+        let str = document.getElementById('shipcoords').value; // Get value from text box
+        setCoords(str); // Run setCoords function and pass str
+        document.getElementById('map').innerHTML = displayMap(); // Run displayMap function
+    });
 }
 
 function setGrid(str){
-    let coords = str.split(" "); //Split input from text box
+    let coords = str.split(" "); // Split input from text box where there is a space
     mapwidth = coords[0];
     mapheight = coords[1];
+}
+
+function setCoords(str){
+    let coords = str.split(" "); // Split input from text box
+    x = coords[0]; // x equals the first coords from text box
+    y = coords[1]; // y equals the second coords from text box
+    orientation = coords[2]; // orientation equals third input from text box
 }
 
 function displayMap(){
@@ -30,12 +40,10 @@ function displayMap(){
     for(let i = 0; i < mapheight; i++){
         for(let j = 0; j < mapwidth; j++){
             if(y==i && x==j){
-                mapString += 'X';
-                // X represents the ship
+                mapString += 'X'; // X represents the ship
             }
             else{
-              mapString = mapString + " . ";  
-              // . represents grid
+              mapString = mapString + " . "; // . represents grid
             }
         }
         mapString = mapString + "</br>"; // Start grid on new line
