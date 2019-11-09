7bf7ba28fc0fd7e4236444cf4f0a32354d1171f5
ewelinasingh <ewelinasingh96@gmail.com>
2019-11-09 21:37:01 +0000

    Create function to direct the ship around the map.

--- script.js
+++ script.js
@@ -20,6 +20,11 @@ window.onload = function(){
         setCoords(str); // Run setCoords function and pass str
         document.getElementById('map').innerHTML = displayMap(); // Run displayMap function
     });
+    document.getElementById('directionbutton').addEventListener('click', e =>{
+        let str = document.getElementById('directions').value;
+        receiveDirections(str);
+        document.getElementById('map').innerHTML = displayMap();
+    });
 }
 
 function setGrid(str){
@@ -33,6 +38,12 @@ function setCoords(str){
     x = coords[0]; // x equals the first coords from text box
     y = coords[1]; // y equals the second coords from text box
     orientation = coords[2]; // orientation equals third input from text box
+}
+
+function receiveDirections(str){
+    for(let i = 0; i < str.length; i++){
+        moveShip(str[i]);
+    }
 }
 
 function displayMap(){
@@ -52,11 +63,39 @@ function displayMap(){
 }
 
 function moveShip(instruction){
-    if(instruction == 'F'){
+    if(instruction == 'L'){
+        if(orientation == 'N'){
+            orientation = 'W'
+        }
+        else if(orientation == 'E'){
+            orientation = 'N';
+        }
+        else if(orientation == 'S'){
+            orientation = 'E';
+        }
+        else if(orientation == 'W'){
+            orientation = 'S';
+        }
+    }
+    else if(instruction == 'R'){
+        if(orientation == 'N'){
+            orientation = 'E'
+        }
+        else if(orientation == 'E'){
+            orientation = 'S';
+        }
+        else if(orientation == 'S'){
+            orientation = 'W';
+        }
+        else if(orientation == 'W'){
+            orientation = 'N';
+        }
+    }
+    else if(instruction == 'F'){
         moveShipForward();
     }
 }
-    
+
 function moveShipForward(){
     if(orientation == 'N'){
         y--;
